8f29d327097b8a958cfeb60228ce9a125376e9fb
jiyou384 <danny384@g.skku.edu>
2023-12-27 14:14:10 +0900

    modify shell script for installing external libraries

--- MPC_control_ex/install.sh
+++ MPC_control_ex/install.sh
@@ -3,31 +3,30 @@ set -e
 
 current_directory=$(pwd)
 
-# cd /usr/include
-# sudo ln -sf eigen3/Eigen Eigen 
-# sudo ln -sf eigen3/unsupported unsupported
-# cd $current_directory
-# echo "Current directory: $current_directory"
+cd /usr/include
+sudo ln -sf eigen3/Eigen Eigen 
+sudo ln -sf eigen3/unsupported unsupported
+cd $current_directory
 
 # clone osqp
-# OSQP_repo="https://github.com/rise-lab-skku-racing/osqp.git"
-# local_path="External/osqp"
-# git clone "$OSQP_repo" "$local_path"
+OSQP_repo="https://github.com/rise-lab-skku-racing/osqp.git"
+local_path="External/osqp"
+git clone "$OSQP_repo" "$local_path"
 
 # clone osqp
 OSQP_eigen_repo="https://github.com/rise-lab-skku-racing/osqp-eigen.git"
 local_path="External/osqp-eigen"
 git clone "$OSQP_eigen_repo" "$local_path"
 
-# cd External/osqp
-# mkdir -p build
-# mkdir -p lib
-# cd build
-# cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$(realpath ../) ..
-# cmake --build .
-# # sudo cmake -DCMAKE_INTALL_PATH=$(realpath ../lib) --build . --target install 
-# sudo cmake --build . --target install
-# cd $current_directory
+cd External/osqp
+mkdir -p build
+mkdir -p lib
+cd build
+mkdir -p install
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$(realpath ../) ..
+cmake --build .
+sudo cmake --build . --target install
+cd $current_directory
 
 cd External/osqp-eigen
 mkdir -p build
